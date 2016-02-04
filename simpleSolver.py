3531bea51b434c94659a39533bd9fcfbf2bd006f
Matt Godfrey <matthewg@silanna.com>
2016-02-05 07:14:32 +1100

    simpleSolver is now calculating bound states correctly (I think. I stilll need to compare with analytic solutions for finite QW). However, the tollerence on the bound state energy isnt tight enough, so the wavefunction still blows up at the end of the simulation domain. This doesnt affect the bound state energy, but it means that I cant normalise my wavefunction (since it diverges). This still needs some more work. I need to see why this wasnt a problem before when I was using the analytic potential.

--- simpleSolver.py
+++ simpleSolver.py
@@ -13,11 +13,11 @@ from scipy.interpolate import interp1d
 def round2int(x):
     return int(x+0.5)
     
-#e=1.6e-19
-e=1
-#m0=9.109e-31
+e=1.6e-19
+#e=1
 hbar=1.05e-34
-m0=hbar**2
+m0=9.109e-31
+#m0=hbar**2
 
 class Structure(object):
     def __init__(self,layers,dx):
@@ -80,6 +80,22 @@ def Wave_function_inf(E, struct):
     psi=Wave_function(E, struct)
     return psi[-1]
     
+def find_root_bisect(f, a, b, tol=1e-12):
+    """
+    finds root of a function f by the bisection method
+    """
+    dx=abs(b-a)
+    while dx>tol:
+        x=(a+b)/2.0
+        if f(a)*f(x)<0:
+            b=x
+        else:
+            a=x
+        dx=abs(b-a)
+        
+    print "found root at x={}".format(x)
+    print "Function on either side is {} - {}".format(f(x-dx),f(x+dx))
+    return x
     
     
 def find_bound_states(struct, do_plots=False):
@@ -90,7 +106,7 @@ def find_bound_states(struct, do_plots=False):
       brentq over this small range to get an accurate value for the zero.
       This is the bound state energy
     """
-    Es = np.linspace(min(struct.V), max(struct.V), 100)   # vector of energies where we look for the stable states
+    Es = np.linspace(min(struct.V), max(struct.V), 20)   # vector of energies where we look for the stable states
     
     psi_inf = []      # vector of wave function at x -> inf (ie, end of domain)
     for e1 in Es:
@@ -102,19 +118,21 @@ def find_bound_states(struct, do_plots=False):
     s = np.sign(psi_inf)
     for i in range(len(psi_inf)-1):
         if s[i]+s[i+1] == 0:
-            zero = brentq(lambda energy: Wave_function_inf(energy, struct), Es[i], Es[i+1])
+            zero = brentq(lambda energy: Wave_function_inf(energy, struct), Es[i], Es[i+1], xtol=1e-80, disp=True, maxiter=100)
+            #zero = find_root_bisect(lambda energy: Wave_function_inf(energy, struct), Es[i], Es[i+1],tol=1e-25)
+            print "E = {} -> psi_inf = {}".format(zero/e, Wave_function_inf(zero,struct))
             E_zeroes.append(zero)
     
     
     if do_plots:
         plt.figure()
-        plt.plot(Es,psi_inf)
+        plt.plot(Es/e,psi_inf)
         plt.title('Values of the $\Psi(\inf)$ vs. Energy')
-        plt.xlabel('Energy, $E$')
+        plt.xlabel('Energy, $E$ [eV]')
         plt.ylabel('$\Psi(x -> inf)$', rotation='horizontal')
         for E in E_zeroes:
-            plt.plot(E, [0], 'go')
-            plt.annotate("E = %.2f"%E, xy = (E, 0), xytext=(E, 30))
+            plt.plot(E/e, [0], 'go')
+            plt.annotate("E = {:.2g}".format(E/e), xy = (E/e, 0), xytext=(E/e, 30))
             plt.grid()    
     
     return E_zeroes
@@ -125,45 +143,46 @@ def normalise(psi,x):
     Normalises a wavefunction.
     Note that this assumes it is a bound state (ie. that the integral is finite)
     """
-    integral=trapz(np.abs(psi),x)
+    integral=trapz(np.abs(psi[:-350]),x[:-350])
     print "integral = {}".format(integral)
     return psi/integral
  
  
 def main():
     # main program        
-    #layers = [[20e-9,10],[10e-9,0],[20e-9,10]]
-    #dx=0.1e-9
-    layers = [[20,10],[1,0],[20,10]]
-    dx=0.1
+    layers = [[5e-9,5],[1e-9,0],[5e-9,5]]
+    dx=0.01e-9
+    #layers = [[20,10],[1,0],[20,10]]
+    #dx=0.1
     struct = Structure(layers,dx)
-    
-    struct.plot_bandstructure()
+    #struct.plot_bandstructure()
 
     bound_states = find_bound_states(struct,True)
     
     print "Bound state energies:"
     for E in bound_states:
-        print " E = {:.2g}".format(E)
-
-
- 
+        print " E = {:.2g}".format(E/e)
 
  
     # Plot the wavefunctions for first 4 eigenstates
-#    col=['r','g','b','c']
-#    plt.figure(2)
-#    plt.plot(x,[V(i) for i in x],'k',label='Potential')
-#    for i,E in enumerate(E_zeroes[0:4]):
-#        psi=Wave_function(E,V,x)
-#        psi=normalise(psi,x)
-#        plt.plot(x, E+psi, label="E = %.2f"%E, color=col[i])
-#        plt.plot(x,E*np.ones(len(x)),'--',label=None,color=col[i])
-#    plt.legend(loc="upper right")
-#    plt.title('Wave function')
-#    plt.xlabel('x, $x/L$')
-#    plt.ylabel('$\Psi(x)$', rotation='horizontal', fontsize = 15)
-#    plt.grid()
+    col=['r','g','b','c','m','y']
+    x=struct.x
+    plt.figure()
+    plt.hold(True)
+    plt.plot(x*1e9,struct.V/e,'k',label='Potential')
+    for i,E in enumerate(bound_states[0:6]):
+        psi=Wave_function(E,struct)
+        psi=normalise(psi,struct.x)
+        plt.plot(x*1e9, E/e+psi, label="E = {:.2f}eV".format(E/e), color=col[i])
+        plt.plot(x*1e9, E/e*np.ones(len(x)), '--', label=None, color=col[i])
+    plt.legend(loc="upper right")
+    plt.title('Wave function')
+    plt.xlabel('x, $x [nm]$')
+    plt.ylabel('$\Psi(x)$', rotation='horizontal', fontsize = 15)
+    plt.grid()
+    #plt.ylim([-10,40])
+ 
+ 
  
 if __name__ == "__main__":
     main()
